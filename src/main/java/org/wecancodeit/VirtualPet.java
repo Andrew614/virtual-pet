e8492d7f047bd0233bcc3530ff8a749e31a454e4
Nick-Hergatt <nickmachergatt@gmail.com>
2019-05-15 13:59:15 -0400

    Test enviroment has no errors, generic methods in production enviroment

---
+++ src/main/java/org/wecancodeit/VirtualPet.java
@@ -0,0 +1,19 @@
+package org.wecancodeit;
+
+public class VirtualPet {
+
+	public VirtualPet(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getHunger() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/test/java/VirtualPetTest.java
+++ src/test/java/VirtualPetTest.java
@@ -1,4 +1,19 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import org.wecancodeit.VirtualPet;
 
 public class VirtualPetTest {
 
+    @Test
+    public void shouldTick() {
+        VirtualPet pet = new VirtualPet("Kendrick");
+
+        int initialHunger = pet.getHunger();
+        pet.tick();
+        int hungerAfterTick = pet.getHunger();
+
+        assertEquals(initialHunger + 10, hungerAfterTick);
+    }
 }
