a9d13301a8ab0adfd5bacf0dd4fad1e082b4310e
BCox330 <cox.815@aol.com>
2019-05-21 14:56:16 -0400

    New Tests Added

--- src/test/java/VirtualPetShelterTest.java
+++ src/test/java/VirtualPetShelterTest.java
@@ -7,12 +7,35 @@ public class VirtualPetShelterTest {
 	@Test
 	public void canAddPetToShelter() {
 		VirtualPetShelter shelter1 = new VirtualPetShelter();
-    VirtualPet pet1 = new VirtualPet("Kendrick");
-    shelter1.addVitrtualPetToShelter(pet1);
-    assertEquals(1,shelter1.getShelterSize());
-    
-    
-    
-    		
+		VirtualPet pet1 = new VirtualPet("Kendrick");
+		shelter1.addVitrtualPetToShelter(pet1);
+		assertEquals(1, shelter1.getShelterSize());
+
+	}
+	@Test
+	public void canAddTwoPetsToShelter() {
+		VirtualPetShelter shelter1 = new VirtualPetShelter();
+		VirtualPet pet1 = new VirtualPet("Kendrick");
+		VirtualPet pet2 = new VirtualPet("FooFoo");
+		shelter1.addVitrtualPetToShelter(pet1);
+		shelter1.addVitrtualPetToShelter(pet2);
+		assertEquals(2, shelter1.getShelterSize());
+		}
+	@Test
+	public void canRemovePetFromShelter() {
+		VirtualPetShelter shelter1 = new VirtualPetShelter();
+		VirtualPet pet1 = new VirtualPet("Kendrick");
+		VirtualPet pet2 = new VirtualPet("FooFoo");
+		shelter1.addVitrtualPetToShelter(pet1);
+		shelter1.addVitrtualPetToShelter(pet2);
+		shelter1.removeVirtualPetFromShelter(pet1);
+		assertEquals(1, shelter1.getShelterSize());
+	}
+	@Test
+	public void canRetrievePetNameFromShelter() {
+		VirtualPetShelter shelter1 = new VirtualPetShelter();
+		VirtualPet pet1 = new VirtualPet("Kendrick");
+		shelter1.addVitrtualPetToShelter(pet1);
+		assertEquals("Kendrick", shelter1.getNameFromList(pet1));
 	}
 }
