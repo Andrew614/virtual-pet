a856a3743b1a95391096cb9730852fb1e6196acf
KaiHennacy <kchennacy@gmail.com>
2019-05-22 12:04:39 -0400

    tests if pet names and age can be retrieved from their map and if multiple pets can beprinted in a list

--- src/test/java/VirtualPetShelterTest.java
+++ src/test/java/VirtualPetShelterTest.java
@@ -1,31 +1,29 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class VirtualPetShelterTest {
+	VirtualPet pet1 = new VirtualPet("Kendrick");
+	VirtualPet pet2 = new VirtualPet("Foo Foo");
+	
+	VirtualPetShelter shelter1 = new VirtualPetShelter();
 
 	@Test
 	public void canAddPetToShelter() {
-		VirtualPetShelter shelter1 = new VirtualPetShelter();
-		VirtualPet pet1 = new VirtualPet("Kendrick");
 		shelter1.addVitrtualPetToShelter(pet1);
 		assertEquals(1, shelter1.getShelterSize());
 
 	}
 	@Test
 	public void canAddTwoPetsToShelter() {
-		VirtualPetShelter shelter1 = new VirtualPetShelter();
-		VirtualPet pet1 = new VirtualPet("Kendrick");
-		VirtualPet pet2 = new VirtualPet("FooFoo");
 		shelter1.addVitrtualPetToShelter(pet1);
 		shelter1.addVitrtualPetToShelter(pet2);
 		assertEquals(2, shelter1.getShelterSize());
 		}
 	@Test
 	public void canRemovePetFromShelter() {
-		VirtualPetShelter shelter1 = new VirtualPetShelter();
-		VirtualPet pet1 = new VirtualPet("Kendrick");
-		VirtualPet pet2 = new VirtualPet("FooFoo");
 		shelter1.addVitrtualPetToShelter(pet1);
 		shelter1.addVitrtualPetToShelter(pet2);
 		shelter1.removeVirtualPetFromShelter(pet1);
@@ -33,10 +31,22 @@ public class VirtualPetShelterTest {
 	}
 	@Test
 	public void canRetrievePetNameFromShelter() {
-		VirtualPetShelter shelter1 = new VirtualPetShelter();
-		VirtualPet pet1 = new VirtualPet("Kendrick");
 		shelter1.addVitrtualPetToShelter(pet1);
-		assertEquals("Kendrick", shelter1.getNameFromList(pet1));
+		String pet1Name = shelter1.getPet("Kendrick").getName();
+		assertEquals("Kendrick", pet1Name);
 	}
-	
+	@Test
+	public void canRetrievePetAgeFromMap() {
+		shelter1.addVitrtualPetToShelter(pet1);
+		int pet1Age = shelter1.getPet("Kendrick").getAge();
+		assertThat(pet1Age, is(0));
+	}
+	@Test
+	public void canListMultiplePets() {
+		shelter1.addVitrtualPetToShelter(pet1);
+		shelter1.addVitrtualPetToShelter(pet2);
+		String petListTest = shelter1.printAllPets();
+		
+		assertThat(petListTest, is("Foo FooKendrick"));
+	}
 }
