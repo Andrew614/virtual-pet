2171ff4043fc4b19e433d4b991624ddfc76b35f4
KaiHennacy <kchennacy@gmail.com>
2019-05-24 10:27:12 -0400

    has ability to choose multiple pets and methods to turn 2d array into a table

--- src/main/java/VirtualPetApp.java
+++ src/main/java/VirtualPetApp.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class VirtualPetApp {
 
@@ -6,72 +8,75 @@ public class VirtualPetApp {
 
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to Virtual Pets Amok");
-		System.out.println("Enter your first pet's name");
+		System.out.println("Enter a pet name");
 		String petName = input.nextLine();
 
 		VirtualPet firstPet = new VirtualPet(petName);
-		VirtualPetShelter shelter1 = new VirtualPetShelter();
-		shelter1.addVitrtualPetToShelter(firstPet);
+		VirtualPetShelter shelter = new VirtualPetShelter();
+		shelter.addVitrtualPetToShelter(firstPet);
 
-		menu(input, firstPet);
+		firstMenu(input, shelter);
+		//secondMenu();
 
 	}
-	
-	/*private static void newMenu(Scanner input, VirtualPetShelter shelter1) {
-		String userChoice = "";
-		//output a table of pets for users to choose from
-		System.out.println("Press 1 to interact with pets");
-		System.out.println("Press 2 to add a pet to the shelter");
-		System.out.println("Press 3 to remove a pet from the shelter");
-		userChoice = input.nextLine();
-		switch (userChoice) {
-		case "1":
-			System.out.println(shelter1.printAllPets());
-			System.out.println("Select one of ^these^ pets to interact with. Press Enter again when finished.");
-			do {
-				
-			}while();
-			break;
-		case "2":
-			break;
-		case "3":
-			break;
-			
-	}*/
 
-	private static void menu(Scanner input, VirtualPet firstPet) {
-		String userChoice = "";
-		do {
-			System.out.println("Enter 1 to feed " + firstPet.getName());
-			System.out.println("Enter 2 to play with " + firstPet.getName());
-			System.out.println("Enter 9 to quit game");
-			userChoice = input.nextLine();
+	public static void firstMenu(Scanner input, VirtualPetShelter shelter) {
+		System.out.println("Your pet has been added to the shelter." + arrayToTable(shelter.petNamesAndStats()));
+	}
 
-			firstPet.tick();
-
-			switch (userChoice) {
-			case "1":
-				firstPet.feed();
-				System.out.println("Your pet's hunger level is " + firstPet.getHunger());
-				System.out.println("Your pet's thirst level is " + firstPet.getThirst() + "\n");
-				break;
-			
-			case "2":
-				firstPet.play();
-				System.out.println("Your pet's boredom level is " + firstPet.getBoredom() + "\n");
-				break;
-
-			case "9":
-				break;
-				
+	private static String arrayToTable(String[][] arr) {
+		String table = "";
+		table += "\n"+makeTableEntry("Name")+makeTableEntry("Hunger")+makeTableEntry("Thirst")+makeTableEntry("Boredom")+makeTableEntry("Age")+"\n";
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				table += makeTableEntry(arr[i][j]);
 			}
-			System.out.println(firstPet.getName() + "'s hunger level is: " + firstPet.getHunger());
-			System.out.println(firstPet.getName() + "'s thirst level is: " + firstPet.getThirst());
-			System.out.println(firstPet.getName() + "'s boredom level is: " + firstPet.getBoredom() + "\n");
-
-		} while (!userChoice.equals("9"));
-
-		System.out.println("Thanks for playing!");
+			table += "\n";
+		}
+		return table;
 	}
+
+
+	private static String makeTableEntry(String str) {
+		String dashes = "";
+		for (int i = 0; i < 10 - Math.floor(str.length() / 2); i++) {
+			dashes += "-";
+		}
+		if (str.length() % 2 == 1) {
+			return "|" + dashes + str + dashes + "-|";
+		}
+		return "|" + dashes + str + dashes +"|";
+
+	}
+
+	/*
+	 * private static void menu(Scanner input, VirtualPet firstPet) { String
+	 * userChoice = ""; do { System.out.println("Enter 1 to feed " +
+	 * firstPet.getName()); System.out.println("Enter 2 to play with " +
+	 * firstPet.getName()); System.out.println("Enter 9 to quit game"); userChoice =
+	 * input.nextLine();
+	 * 
+	 * firstPet.tick();
+	 * 
+	 * switch (userChoice) { case "1": firstPet.feed();
+	 * System.out.println("Your pet's hunger level is " + firstPet.getHunger());
+	 * System.out.println("Your pet's thirst level is " + firstPet.getThirst() +
+	 * "\n"); break;
+	 * 
+	 * case "2": firstPet.play(); System.out.println("Your pet's boredom level is "
+	 * + firstPet.getBoredom() + "\n"); break;
+	 * 
+	 * case "9": break;
+	 * 
+	 * } System.out.println(firstPet.getName() + "'s hunger level is: " +
+	 * firstPet.getHunger()); System.out.println(firstPet.getName() +
+	 * "'s thirst level is: " + firstPet.getThirst());
+	 * System.out.println(firstPet.getName() + "'s boredom level is: " +
+	 * firstPet.getBoredom() + "\n");
+	 * 
+	 * } while (!userChoice.equals("9"));
+	 * 
+	 * System.out.println("Thanks for playing!"); }
+	 */
 
 }
